091188727dabac112ae821ea631d58759401e56b
jiya <jiyagartaula@gmail.com>
2025-10-16 18:03:19 +0545

    adding productcontroller

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,14 @@
 <?php
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/user', [AuthController::class, 'getUser']);
 Route::put('/user', [AuthController::class, 'update']); 
 
+Route::post('/products', [ProductController::class, 'store']);
 
-
+Route::get('/products', [ProductController::class, 'index']);
+Route::put('/products/{product}', [ProductController::class, 'update']);
+Route::delete('/products/{product}', [ProductController::class, 'destroy']);

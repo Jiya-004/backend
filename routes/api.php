6fcca8537b582aeb0b597990d2689e6e7e9c8a27
utsha <thapautsha823@gmail.com>
2025-10-16 19:39:10 +0545

    Contact message

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,14 @@
 <?php
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ContactMessageController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/user', [AuthController::class, 'getUser']);
 Route::put('/user', [AuthController::class, 'update']); 
-
+Route::post('/contact', [ContactMessageController::class, 'store']);
+Route::get('/contact', [ContactMessageController::class, 'index']); 
 Route::post('/products', [ProductController::class, 'store']);
 
 Route::get('/products', [ProductController::class, 'index']);

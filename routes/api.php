69054f62c892db1730899d6fdc6940ccb683e34b
jiya <jiyagartaula@gmail.com>
2025-10-17 09:11:56 +0545

    changing productcontroller

--- routes/api.php
+++ routes/api.php
@@ -14,3 +14,4 @@ Route::post('/products', [ProductController::class, 'store']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::put('/products/{product}', [ProductController::class, 'update']);
 Route::delete('/products/{product}', [ProductController::class, 'destroy']);
+Route::get('/products', [ProductController::class, 'index']);
